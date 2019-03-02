3cce1779d870149f3c1df4a8a85bd737316e7c54
ArtyomResh <artyom.resh@csssr.com>
2019-03-02 05:26:17 +0300

    Updates

--- scripts/dom-manipulations.js
+++ scripts/dom-manipulations.js
@@ -1,19 +1,33 @@
+let counter_1 = 1
+let counter_2 = 1
+
 function addCodeGoodPracrice() {
+    counter_1 = ++counter_1
     const listItemOne = document.getElementsByClassName("list-item")[0];
     listItemOne.textContent = "Элемент списка 1";
 
     const list = document.getElementsByClassName("list_one")[0];
-
     const listItemTwo = document.createElement("li");
+
     listItemTwo.classList.add("list-item");
-    listItemTwo.textContent = "Элемент списка 2";
+    listItemTwo.textContent = `Элемент списка ${counter_1}`;
     list.appendChild(listItemTwo);
 }
 
 function addCodeBadPracrice() {
+  counter_2 = ++counter_2
   const list = document.getElementsByClassName("list_two")[0];
-  list.innerHTML = list.innerHTML + `
-    <li class="list-item">Элемент списка 1</li>
-    <li class="list-item">Элемент списка 2</li>
-  `;
+
+  if (counter_2 === 2) {
+      list.innerHTML = `
+        <li class="list-item">Элемент списка 1</li>
+        <li class="list-item">Элемент списка ${counter_2}</li>
+      `
+  } else {
+      list.innerHTML = list.innerHTML + `
+        <li class="list-item">Элемент списка ${counter_2}</li>
+      `;
+  }
+
+
 }
